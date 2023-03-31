de024abca358374ebf6c1ec73b3f18dd83bb2fb9
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-03-31 14:02:21 +0100

    added css and javascript

---
+++ assets/script/script.js
@@ -0,0 +1,21 @@
+const start = document.getElementById("AC");
+const plusminus = document.getElementById("+-");
+const pourcentage = document.getElementById("%");
+const division = document.getElementById("/");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+const multiplication = document.getElementById("*");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const subtraction = document.getElementById("-");
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const addition = document.getElementById("+");
+const zero = document.getElementById("0");
+const point = document.getElementById(".");
+const equality = document.getElementById("=");
+
+// andela personal evaluation form
