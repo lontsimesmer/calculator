f82b288a9137b30911f1b3c10509d37cb5b157f8
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-04-01 03:03:41 +0100

    solve linter errors

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,217 +1,100 @@
-const input = document.getElementById('input')
-const result = document.getElementById('result')
-const number = document.querySelectorAll('.numbers')
-const operator = document.querySelectorAll('.operators')
-const displayResults = false
+const input = document.getElementById("input");
+const result = document.getElementById("result");
+const number = document.querySelectorAll(".numbers");
+const operator = document.querySelectorAll(".operators");
+const displayResults = false;
 
 for (let i = 0; i < number.length; i++) {
-  number[i].addEventListener('click', function (e) {
-    const currentString = input.innerHTML
-    const lastCharacter = currentString[currentString.length - 1]
+  number[i].addEventListener("click", function (e) {
+    const currentString = input.innerHTML;
+    const lastCharacter = currentString[currentString.length - 1];
 
     if (displayResults === false) {
-      input.innerHTML += e.target.innerHTML
+      input.innerHTML += e.target.innerHTML;
     } else if (
-      (displayResults === true && lastCharacter === '÷') ||
-      lastCharacter === '*' ||
-      lastCharacter === '-' ||
-      lastCharacter === '+'
+      (displayResults === true && lastCharacter === "÷") ||
+      lastCharacter === "*" ||
+      lastCharacter === "-" ||
+      lastCharacter === "+"
     ) {
-      displayResults = false
-      input.innerHTML += e.target.innerHTML
+      displayResults = false;
+      input.innerHTML += e.target.innerHTML;
     } else {
-      displayResults = false
-      input.innerHTML = '0'
-      input.innerHTML += e.target.innerHTML
+      displayResults = false;
+      input.innerHTML = "0";
+      input.innerHTML += e.target.innerHTML;
     }
-    console.log(displayResults)
-  })
+    console.log(displayResults);
+  });
 }
 
 for (let i = 0; i < operator.length; i++) {
-  operator[i].addEventListener('click', function (e) {
-    const currentString = input.innerHTML
-    const lastOperator = currentString[currentString.length - 1]
+  operator[i].addEventListener("click", function (e) {
+    const currentString = input.innerHTML;
+    const lastOperator = currentString[currentString.length - 1];
     if (
-      lastOperator === '÷' ||
-      lastOperator === '*' ||
-      lastOperator === '-' ||
-      lastOperator === '+'
+      lastOperator === "÷" ||
+      lastOperator === "*" ||
+      lastOperator === "-" ||
+      lastOperator === "+"
     ) {
       const newString =
         currentString.substring(0, currentString.length - 1) +
-        e.target.innerHTML
-      input.innerHTML = newString
-    } else if (currentString === 0) {
-      console.log('enter any false number')
-    } else {
-      input.innerHTML += e.target.innerHTML
-    }
-  })
-}
-
-result.addEventListener('click', function () {
-  const inputString = this.innerHTML
-  const numbers = inputString.split(/\÷|\*|\-|\+/g)
-  const operators = inputString.replace(/[0-9]|\./g, '').split('')
-  console.log(inputString)
-  console.log(numbers)
-  console.log(operators)
-
-  const divide = operators.indexOf('÷')
-  while (divide != -1) {
-    numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1])
-    operators.splice(divide, 1)
-    divide = operators.indexOf('÷')
-  }
-
-  const multiply = operators.indexOf('*')
-  while (multiply != -1) {
-    numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1])
-    operators.splice(multiply, 1)
-    multiply = operators.indexOf('*')
-  }
-
-  const subtract = operators.indexOf('-')
-  while (subtract != -1) {
-    numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1])
-    operators.splice(subtract, 1)
-    subtract = operators.indexOf('-')
-  }
-
-  const add = operators.indexOf('+')
-  while (add != -1) {
-    numbers.splice(
-      add,
-      2,
-      parseFloat(numbers[add]) + parseFloat(numbers[add + 1])
-    )
-    operators.splice(add, 1)
-    add = operators.indexOf('+')
-  }
-
-  input.innerHTML = numbers[0]
-
-  displayResults = true
-})
-
-// andela personal evaluation form
-
-/* var input = document.getElementById('input'), // input/output button
-  number = document.querySelectorAll('.numbers div'), // number buttons
-  operator = document.querySelectorAll('.operators div'), // operator buttons
-  result = document.getElementById('result'), // equal button
-  clear = document.getElementById('clear'), // clear button
-  resultDisplayed = false; // flag to keep an eye on what output is displayed
-
-// adding click handlers to number buttons
-for (var i = 0; i < number.length; i++) {
-  number[i].addEventListener("click", function(e) {
-
-    // storing current input string and its last character in variables - used later
-    var currentString = input.innerHTML;
-    var lastChar = currentString[currentString.length - 1];
-
-    // if result is not diplayed, just keep adding
-    if (resultDisplayed === false) {
-      input.innerHTML += e.target.innerHTML;
-    } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-      // if result is currently displayed and user pressed an operator
-      // we need to keep on adding to the string for next operation
-      resultDisplayed = false;
-      input.innerHTML += e.target.innerHTML;
-    } else {
-      // if result is currently displayed and user pressed a number
-      // we need clear the input string and add the new input to start the new opration
-      resultDisplayed = false;
-      input.innerHTML = "";
-      input.innerHTML += e.target.innerHTML;
-    }
-
-  });
-}
-
-// adding click handlers to number buttons
-for (var i = 0; i < operator.length; i++) {
-  operator[i].addEventListener("click", function(e) {
-
-    // storing current input string and its last character in variables - used later
-    var currentString = input.innerHTML;
-    var lastChar = currentString[currentString.length - 1];
-
-    // if last character entered is an operator, replace it with the currently pressed one
-    if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-      var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+        e.target.innerHTML;
       input.innerHTML = newString;
-    } else if (currentString.length == 0) {
-      // if first key pressed is an opearator, don't do anything
-      console.log("enter a number first");
+    } else if (currentString === 0) {
+      console.log("enter any false number");
     } else {
-      // else just add the operator pressed to the input
       input.innerHTML += e.target.innerHTML;
     }
-
   });
 }
 
-// on click of 'equal' button
-result.addEventListener("click", function() {
-
-  // this is the string that we will be processing eg. -10+26+33-56*34/23
-  var inputString = input.innerHTML;
-
-  // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-  var numbers = inputString.split(/\+|\-|\×|\÷/g);
-
-  // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
-  // first we replace all the numbers and dot with empty string and then split
-  var operators = inputString.replace(/[0-9]|\./g, "").split("");
-
+result.addEventListener("click", function () {
+  const inputString = this.innerHTML;
+  const numbers = inputString.split(/\÷|\*|\-|\+/g);
+  const operators = inputString.replace(/[0-9]|\./g, "").split("");
   console.log(inputString);
-  console.log(operators);
   console.log(numbers);
-  console.log("----------------------------");
+  console.log(operators);
 
-  // now we are looping through the array and doing one operation at a time.
-  // first divide, then multiply, then subtraction and then addition
-  // as we move we are alterning the original numbers and operators array
-  // the final element remaining in the array will be the output
-
-  var divide = operators.indexOf("÷");
-  while (divide != -1) {
+  const divide = operators.indexOf("÷");
+  while (divide !== -1) {
     numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
     operators.splice(divide, 1);
     divide = operators.indexOf("÷");
   }
 
-  var multiply = operators.indexOf("×");
-  while (multiply != -1) {
+  const multiply = operators.indexOf("*");
+  while (multiply !== -1) {
     numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
     operators.splice(multiply, 1);
-    multiply = operators.indexOf("×");
+    multiply = operators.indexOf("*");
   }
 
-  var subtract = operators.indexOf("-");
-  while (subtract != -1) {
+  const subtract = operators.indexOf("-");
+  while (subtract !== -1) {
     numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
     operators.splice(subtract, 1);
     subtract = operators.indexOf("-");
   }
 
-  var add = operators.indexOf("+");
-  while (add != -1) {
-    // using parseFloat is necessary, otherwise it will result in string concatenation :)
-    numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+  const add = operators.indexOf("+");
+  while (add !== -1) {
+    numbers.splice(
+      add,
+      2,
+      parseFloat(numbers[add]) + parseFloat(numbers[add + 1])
+    );
     operators.splice(add, 1);
     add = operators.indexOf("+");
   }
 
-  input.innerHTML = numbers[0]; // displaying the output
+  input.innerHTML = numbers[0];
 
-  resultDisplayed = true; // turning flag if result is displayed
+  displayResults = true;
 });
 
-// clearing the input on press of clear
-clear.addEventListener("click", function() {
+clear.addEventListener("click", function () {
   input.innerHTML = "";
-}) */
+});
